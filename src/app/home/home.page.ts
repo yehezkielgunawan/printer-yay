247bb1fa9499ff294e10e6b10e58926b5c4b9267
Yehezkiel Gunawan <yehezkiel.gunawan@codify.id>
2020-09-14 16:47:56 +0700

    test print with window print and remove ng IF from HTML

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,7 +19,7 @@ export class HomePage {
     var WinPrint = window.open(
       "",
       "",
-      "left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0"
+      "left=0,top=0,width=250,height=300,toolbar=0,scrollbars=0,status=0"
     );
     WinPrint.document.write(prtContent.innerHTML);
     WinPrint.document.close();
